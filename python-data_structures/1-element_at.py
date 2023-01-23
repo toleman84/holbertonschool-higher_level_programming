abf379007fe65f50c74a685dbbfda1ae2f16b967
toleman84 <gdponcemarsiglia@gmail.com>
2023-01-23 05:13:09 -0800

    tasks

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -7,8 +7,3 @@ def element_at(my_list, idx):
         return None
     else:
         return my_list[idx]
-
-#    if idx < 0 or idx >= len(my_list):
-#        return None
-#    else:
-#        return(my_list[idx])
