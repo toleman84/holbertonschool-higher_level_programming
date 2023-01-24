3d51e20268b0c9b92e7c2a6ac64f96e0f0c5565d
toleman84 <gdponcemarsiglia@gmail.com>
2023-01-24 08:02:41 -0800

    tasks

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
 
-#    new_dictionary = a_dictionary.copy()
-#    [new_dictionary.update((key, value * 2) for key, value in new_dictionary.items())]
-#    return new_dictionary
-
     new = {}
     for key, value in a_dictionary.items():
         new.update({key: value * 2})
     return new
-
