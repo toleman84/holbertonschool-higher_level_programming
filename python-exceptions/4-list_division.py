0f4168c26ef5e914479e6e642729462040221183
toleman84 <gdponcemarsiglia@gmail.com>
2023-01-25 07:48:32 -0800

    tasks

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,6 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
 
     new_list = []
     for i in range(list_length):
+        division = 0
         try:
             division = my_list_1[i] / my_list_2[i]
         except (ZeroDivisionError, ValueError):
