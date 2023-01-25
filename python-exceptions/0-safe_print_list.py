c91f2a882a7097886627e73d6a9eb2763197db0f
toleman84 <gdponcemarsiglia@gmail.com>
2023-01-25 05:31:57 -0800

    tasks

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    count = 0
+
+    while count < x:
+        try:
+            print('{}'.format(my_list[count]), end='')
+            count += 1
+        except IndexError:
+            print()
+            return count
+    print()
+    return count
+
