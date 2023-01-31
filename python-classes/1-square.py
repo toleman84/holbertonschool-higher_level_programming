fe0cc276b4af6616ec887f6dae184a5d141a0b7b
toleman84 <gdponcemarsiglia@gmail.com>
2023-01-31 08:44:18 -0800

    tasks

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+1. Square with size
+defines a square
+"""
+
+
 class Square:
+    """ class Square difinition """
     def __init__(self, size):
         """ size of a side of square """
         self.__size = size
