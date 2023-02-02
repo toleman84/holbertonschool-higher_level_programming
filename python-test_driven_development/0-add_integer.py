b360a4f97c5a0000ce9c35f17f4843da6994bca2
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-02 08:39:29 -0800

    tasks

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-
+"""
+0. Integers addition
+"""
 
 def add_integer(a, b=98):
-
+    """ must be first casted to integers if they are float """
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
 
+    """ Returns an integer: the addition of a and b """
     try:
         return a + b
     except TypeError:
