9bcf49b218029cbbe5f39a1fbe063eef00c167eb
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-02 05:32:17 -0800

    tasks

---
+++ python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+6. Coordinates of a square
+"""
+
+
+class Square:
+    """ init function """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ size of a side of square """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return(self.__position)
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    """ Public instance method """
+    def area(self):
+        return(self.__size**2)
+
+    """  that prints in stdout the square with the character # """
+    def my_print(self):
+        [print('') for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(' ', end='') for j in range(0, self.__position[0])]
+            [print('#', end='') for k in range(0, self.__size)]
+            print('')
+
+        if self.__size == 0:
+            print('')
