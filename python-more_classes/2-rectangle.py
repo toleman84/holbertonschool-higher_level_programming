2c2ecafc28f85647c30bfe8ee4d84c319eaa9481
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-05 12:46:48 -0800

    tasks

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+2. Area and Perimeter
+
+"""
+
+
+class Rectangle:
+
+    """ Private instance attribute: width: """
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    """ Private instance attribute: height: """
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    """ Instantiation with optional width and height: """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """ Public instance method: """
+
+    def area(self):
+        return self.height * self.width
+
+    """ Public instance method: """
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.height * 2) + (self.width * 2)
