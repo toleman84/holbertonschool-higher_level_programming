717acd784bbc18e6f18451168bae809d3cc1ae1b
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-04 19:10:51 -0800

    tasks

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -16,8 +16,15 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(max_integer([None]), None)
 
-#    def test_str(self):
-#        self.assertEqual(max_integer(["Chateau", "the", "cat", "is"]), "Chateau")
+    def test_str(self):
+        self.assertEqual(max_integer(["Chateau", "the", "cat", "is"]), "Chateau")
+
+    def test_empty(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_max_at_begginning(self):
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_beginning), 4)
 
 if __name__ == '__main__':
     unittest.main()
