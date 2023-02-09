92280e73baab212219154ab8ef0bc7e1f513ec08
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-09 06:12:15 -0800

    tasks

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+6. Improve Geometry
+
+"""
+
+
+class BaseGeometry:
+    """ public instance method """
+    def area(self):
+        """ if not implemented """
+        raise Exception(area() is not implemented)
