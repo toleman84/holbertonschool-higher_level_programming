7c4dc6e60056cdd08912341fbd0f8f4fe551b59f
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-09 05:57:00 -0800

    tasks

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+4. Only sub class of
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """ if the object is an instance of a class that inherited """
+    if isinstance(obj, a_class):
+        return True
+    return False
