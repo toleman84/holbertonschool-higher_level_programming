06f39ab5160f148fcfb824198227b80bb8538352
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-09 06:13:47 -0800

    tasks

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -9,4 +9,4 @@ class BaseGeometry:
     """ public instance method """
     def area(self):
         """ if not implemented """
-        raise Exception(area() is not implemented)
+        raise Exception('area() is not implemented')
