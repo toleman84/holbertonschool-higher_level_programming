14a3d0b1c1ec191294e1cf990f60e1da9f83c9f2
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-09 05:30:41 -0800

    tasks

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,8 +5,8 @@
 """
 
 
-class MyList:
+class MyList(list):
 
     def print_sorted(self):
-    """ that prints the list, but sorted (ascending sort) """
-    print(sorted(self))
+        """ that prints the list, but sorted (ascending sort) """
+        print(sorted(self))
---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+2. Exact same object
+
+"""
+
+
+def is_same_class(obj, a_class):
+    """ that returns True or False if ... """
+    if type(obj) == a_class:
+        return True
+    return False
