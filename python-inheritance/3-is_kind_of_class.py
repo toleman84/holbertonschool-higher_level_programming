9a331b9af1b985e3ebb1a933a531b400d8b4f5af
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-09 05:36:14 -0800

    tasks

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+3. Same class or inherit from
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """  if the object is an instance of: """
+    if isinstance(obj, a_class):
+        return True
+    return False
