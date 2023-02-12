8bc1e8ed7bb0aa7ff2e5f9330af2fd25674bbd64
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-12 13:30:56 -0800

    tasks

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,17 +5,21 @@
 """
 import json
 import sys
+import os.path
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-args = sys.argv
-filename = 'add_item.json'
-
-with open(filename, 'a+', encoding='utf-8') as f:
-    my_list = []
-    my_list.extend(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+if os.path.isfile('add_item.json'):
+    my_lists = load_from_json_file('add_item.json')
+    my_lists.extend(sys.argv[1:])
+    save_to_json_file(my_lists, 'add_item.json')
+    load_from_json_file('add_item.json')
+else:
+    with open('add_item.json', 'a+', encoding='utf-8') as f:
+        my_list = []
+        my_list.extend(sys.argv[1:])
+        save_to_json_file(my_list, 'add_item.json')
+        load_from_json_file('add_item.json')
