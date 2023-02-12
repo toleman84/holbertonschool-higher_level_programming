d025ae4e14251d35c51d181dc562dad835d7abe7
toleman84 <gdponcemarsiglia@gmail.com>
2023-02-12 10:58:57 -0800

    tasks

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+3. To JSON string
+
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ returns the JSON representation of an object """
+    return json.dumps(my_obj)
