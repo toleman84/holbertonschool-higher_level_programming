41a2aafca966950e94b2919c7d593f655f35670c
Gustavo <gdponcemarsiglia@gmail.com>
2023-02-18 07:34:16 -0800

    tasks

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ import csv
 
 
 class Base:
-
+    """ private class attribute """
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -19,5 +19,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-
