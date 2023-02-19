72e4d47e0d7849025fb7578a9403c29e927606a0
Gustavo <gdponcemarsiglia@gmail.com>
2023-02-19 13:51:12 -0800

    tasks

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+test base
+
+"""
+
+import os
+import unittest
+
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class Test_instantiation(unittest.TestCase):
+
+    """
+    example:
+
+    def test_upper(self):
+        self.asserEqual('foo'.upper(), 'FOO')
+
+    """
+
+    def test_no_arg(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+
+class Test_to_json_string(unittest.TestCase):
+
+    def test_to_json_string(self):
+        s = Square(10, 2, 3, 4)
+        self.assertEqual(str, type(Base.to_json_string([s.to_dictionary()])))
+
+
+class Test_save_to_file(unittest.TestCase):
+
+    def test_save_to_file(self):
+        Square.save_to_file([])
+        with open("Square.json", "r") as f:
+            self.assertEqual("[]", f.read())
+
+
+class Test_from_json_string(unittest.TestCase):
+
+    def test_from_json_string(self):
+        self.assertEqual([], Base.from_json_string(None))
+
+
+class Test_create(unittest.TestCase):
+
+    def test_create(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r1))
+
+
+class Test_load_from_file(unittest.TestCase):
+
+    def test_load_from_file(self):
+        output = Square.load_from_file()
+        self.assertEqual([], output)
+
+"""
+class Test_save_to_file_csv(unittest.TestCase):
+
+
+class Test_load_from_file_csv(unittest.TestCase):
+"""
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+test rectangle
+
+"""
+
+import unittest
+import json
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
