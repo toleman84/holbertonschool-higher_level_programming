71396ee08569c129f66805af9c353976b3e790ee
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 13:12:34 -0300

    fix ej 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(9):
-    for j in range(10):
-        if i != j:
-            print("{}{}".format(i, j), end=", ")
+    for j in range(i + 1, 10):
+        print("{}{}".format(i, j), end=", ")
