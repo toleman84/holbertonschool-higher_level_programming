c6192179737e111e4092c0a6d22353caa2e2abdf
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 15:15:07 -0300

    fix ej 100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for letters in range(122, 96, -1):
     if letters % 2 == 0:
-        print(chr(letters))
+        print(chr(letters), end="")
     else:
         letter = letters - 32
-        print(chr(letter))
+        print(chr(letter), end="")
