741d3d0bf39dbea19e52f8046d33581eb845399e
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 11:07:10 -0300

    fix ej 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    print("{}".format(number), end=", ")
+    if number < 99:
+        print("{}".format(number), end="")
+        print(", ", end="")
+    elif number == 98:
+        print("", end="")
