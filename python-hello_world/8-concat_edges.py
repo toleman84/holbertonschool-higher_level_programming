db9a5ef58e6214d9744e51f44a8d2ee30c63d8d8
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-30 22:04:49 -0300

    add ej 6

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[40:55] + str[108:114] + str[0:6]
+print(str)
+
+
+#object-oriented programming with Python
