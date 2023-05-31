b3e7c1b547eb5d61c3753a78209f93df7a58ae26
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 13:38:07 -0300

    fix ej 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for letter in c:
-        if ord(letter) >= 97 and ord(letter) <= 122:
+        if ord(c) >= 97 and ord(c) <= 122:
             return True
         else:
             return False
