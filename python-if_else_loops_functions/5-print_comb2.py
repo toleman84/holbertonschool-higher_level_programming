d7f6c136bcc9332a999719505fc242a7ffc39a94
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 12:54:18 -0300

    fix ej 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for number in range(100):
-    print("{0:0=2d}".format(number), end="")
+    print("{0:0=2d}".format(number), end=", ")
     if number < 99:
-        print(", ", end="")
+        print("99")
