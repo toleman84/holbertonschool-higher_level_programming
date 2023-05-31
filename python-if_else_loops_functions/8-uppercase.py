d14b204ea6b70f4e7a549e4626df6408db2fcac0
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 14:04:22 -0300

    fix ej 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for to_up in str:
         if ord(to_up) >= 97 and ord(to_up) <= 122:
-            # to_up = chr(ord(to_up) - 32)
-            print("{}".format(chr(ord(to_up) - 32)), end="")
+            to_up = chr(ord(to_up) - 32)
+        print("{}".format(to_up), end="")
     print("")
