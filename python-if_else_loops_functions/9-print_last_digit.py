31a9a588129c73ece8adf6224b28d7797abc4cc6
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 14:23:12 -0300

    fix ej 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        return number % -10
-    else:
-        return number % 10
+    last_digit = abs(number)
+    print("{}".format(last_digit))
+    return last_digit
