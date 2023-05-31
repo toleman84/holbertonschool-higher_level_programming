1fd69a5164510e7428b65af8c47d39a921f9dd2c
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 14:33:21 -0300

    add ej 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def fizzbuzz():
+    for number in range(101):
+        if number % 3:
+            print("Fizz")
+        elif number % 5:
+            print("Buzz")
+        elif number % 15:
+            print("FizzBuzz")
