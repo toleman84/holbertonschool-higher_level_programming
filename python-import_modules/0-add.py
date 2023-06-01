2f0f3acf470c35d4b5d28c98c8f52cda24f6cc99
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 21:13:26 -0300

    change file 0-add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
-sum = add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+def add(a, b):
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
