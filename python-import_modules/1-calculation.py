97ca668b3378f23fb96bdbde5d74b8a454d3f475
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 21:45:31 -0300

    fix ej 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,5 +3,15 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    
-    print("{} + {} = {}".format(a, b, sum = add(a, b)))
+
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
+
+    res = sub(a, b)
+    print("{} - {} = {}".format(a, b, res))
+
+    multi = mul(a, b)
+    print("{} * {} = {}".format(a, b, multi))
+
+    div2 = div(a, b)
+    print("{} / {} = {}".format(a, b, div2))
