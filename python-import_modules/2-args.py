e7e57f58ef5bc06f0137e21a5ad16be31d54959a
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 23:26:37 -0300

    fix ej 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,10 @@ if __name__ == "__main__":
 
     if lengh == 1:
         print("0 arguments.")
-    elif lengh > 1:
+    elif lengh == 2:
         print("{} argument:".format(lengh - 1))
+        print()
+    elif lengh > 2:
+        print("{} arguments:".format(lengh - 1))
         for lengh_arg in range(1, lengh):
             print("{}: {}".format(lengh_arg, arg))
