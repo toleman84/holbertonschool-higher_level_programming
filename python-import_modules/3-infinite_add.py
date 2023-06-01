e72ffbd29bf6eae7a29f2aa484aae92aaedf158c
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-01 00:27:36 -0300

    fix ej 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import sys
     numbers = len(sys.argv)
+    sum = 0
     for number in range(1, numbers):
         sum += int(sys.argv[number])
     print("{}".format(sum))
