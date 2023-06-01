1e12a0257e070e1b984cb44ce4e050071bf1de94
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-31 23:30:40 -0300

    fix ej 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
     lengh = len(sys.argv)
@@ -14,4 +13,4 @@ if __name__ == "__main__":
     elif lengh > 2:
         print("{} arguments:".format(lengh - 1))
         for lengh_arg in range(1, lengh):
-            print("{}: {}".format(lengh_arg, arg))
+            print("{}: {}".format(lengh_arg, argv[lengh_arg]))
