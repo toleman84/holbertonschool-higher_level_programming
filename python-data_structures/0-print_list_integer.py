07c76af3a3e7b930fb8139f30816027501e1c364
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-06 00:03:58 -0300

    fix ej 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""comments"""
-
-
 def print_list_integer(my_list=[]):
-    """print all integer of a list"""
-    str = "{}"
     for i in my_list:
-        print(str.format(i))
+        print({}.format(i))
