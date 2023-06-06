55b9e756bf874c3c7da8745a859977c1e686fe9e
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-05 23:39:00 -0300

    fix ej 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for i in range(my_list):
-        print(my_list.format(i))
+    # for i in range(my_list):
+        print(format(my_list))
