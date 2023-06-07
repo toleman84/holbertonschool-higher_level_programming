7f563ca3ffed69d8216c5fd9c1805aacd5e1460b
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-07 00:04:26 -0300

    fix ej 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -9,6 +9,6 @@ def element_at(my_list, idx):
         print("2")
         return None
     #Write a function that retrieves an element from a list.
-    for i in my_list:
+    for i in range(my_list):
         j = i[idx]
         print("Element at index {:d} is {}".format(idx, j))
