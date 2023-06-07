08596b94dbee3c4a0bbeb4ad2d6ba35a1ebea20e
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-07 00:45:04 -0300

    fix ej 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    #rev = i[::-1]
-    print(format(my_list))
+    for i in my_list:
+        print("{:d}".format(i))
