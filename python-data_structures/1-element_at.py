fe1d22ae31302c2ac00e3e5a89cb7cbbf445c1c7
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-06 23:59:28 -0300

    fix ej 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,9 +2,11 @@
 def element_at(my_list, idx):
     #If idx is negative, the function should return None
     if idx < 0:
+        print("1")
         return None
     #If idx is out of range (> of number of element in my_list), the function should return None
     if idx > len(my_list):
+        print("2")
         return None
     #Write a function that retrieves an element from a list.
 
