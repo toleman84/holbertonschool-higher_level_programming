682f517dbf70453f7d996bf5847ace0c65cb5ad0
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-07 12:00:31 -0300

    fix ej 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,6 +6,6 @@ def new_in_list(my_list, idx, element):
     if idx >= len(my_list):
         return my_list
     else:
-        new_list = my_list.copy()
-        new_list[idx] = element
+        my_list = list.copy(my_list)
+        my_list[idx] = element
         return my_list
