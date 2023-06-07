17211d365044f02bcab6b242aad716203d4b40c9
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-07 14:53:52 -0300

    fix ej 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,6 +3,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
 
+    max = my_list[0]
     for x in my_list:
-        if x > my_list[0]:
-            return x
+        if x > max:
+            max = x
+    return max
