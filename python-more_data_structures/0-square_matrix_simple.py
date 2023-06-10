979c6f36f6e479e8ca2345fa6b6ef8626388a1e1
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-10 16:28:10 -0300

    fix ej 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,8 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in matrix:
+        new_matrix2 = []
         for j in i:
-            new_matrix.append(j ** 2)
+            new_matrix2.append(j ** 2)
+        new_matrix.append(new_matrix2)
     print(new_matrix)
