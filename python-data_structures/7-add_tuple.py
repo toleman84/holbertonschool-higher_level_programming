a516eaeeea4ad46da3d9823dfeaa18c29600baad
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-10 14:30:26 -0300

    fix ej 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2 or len(tuple_b) < 2:
         #use the value 0 for each missing integer
         
-        
     if len(tuple_a) > 2 or len(tuple_b) < 2:
         #use only the first 2 integers.
-        
+
+    return ((tuple_a[0] + tuple_b[0])(tuple_a[1] + tuple_b[1]))
