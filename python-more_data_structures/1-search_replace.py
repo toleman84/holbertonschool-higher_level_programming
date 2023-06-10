ae5e12c29cf7b2acbf350cc08ae7e47baa27cbc9
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-10 16:39:33 -0300

    add ej 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    #function that replaces all occurrences of an element by another in a new list.
+    new_list = []
+    
+    if search >= len(my_list):
+        return my_list
+    else:
+        new_list[search] = replace
+        return new_list
+
+    #my_list is the initial list
+    #search is the element to replace in the list
+    #eplace is the new element
