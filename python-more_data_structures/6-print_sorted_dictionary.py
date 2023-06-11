d5f14dac17553960a56465929701268e36b9933b
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-10 23:31:45 -0300

    fix ej 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for i in a_dictionary:
-        sorted_dictionary = sorted(a_dictionary(), key=lambda x:x[1])
+        sorted_dictionary = sorted(a_dictionary.items(), key=lambda x:x[1])
     print(sorted_dictionary)
