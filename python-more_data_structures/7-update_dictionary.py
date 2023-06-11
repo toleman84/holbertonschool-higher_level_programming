153d4d639dfc4676d3cba4ff9e31199fbf366cfb
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-11 00:24:32 -0300

    fix ej 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     new_dict = []
-    for k, v in a_dictionary:
+    for k in a_dictionary:
         if k == a_dictionary:
             new_dict[k].append(key)
         #if v == a_dictionary:
