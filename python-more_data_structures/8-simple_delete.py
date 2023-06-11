afeca99a247a1526da7fb27c047a5d35855da319
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-11 00:43:02 -0300

    fix ej 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if not key:
+    if not key in a_dictionary:
         return a_dictionary
-    a_dictionary.update({key})
-    return a_dictionary
+    else:
+        a_dictionary.pop({key})
+        return a_dictionary
