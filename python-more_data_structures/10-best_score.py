67a7c43fd48028fb23a6995eb81350fb76bdafe5
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-11 16:02:05 -0300

    add ej 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
-    max_score = max(a_dictionary)
+    max_score = max(a_dictionary, key=lambda x: a_dictionary[x])
     return max_score
