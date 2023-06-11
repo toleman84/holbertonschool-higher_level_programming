c3dc8c6ebb5d46be7dbde9661850034a4506a98e
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-11 00:21:55 -0300

    fix ej 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     new_dict = []
-    for k, v in a_dictionary:
+    for k in a_dictionary:
         new_dict[k].append(key)
-        new_dict[v].append(value)
+        for v in a_dictionary:
+            new_dict[v].append(value)
     print(new_dict)
 
 #new_list.append(replace)
