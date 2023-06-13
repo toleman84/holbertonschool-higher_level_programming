b7469d3eaca9457628fb47866f30f0340798827d
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-13 16:07:52 -0300

    fix ej 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -41,6 +41,6 @@ class Square:
             print("")
         else:
             for i in range(self.__size):
+                print("#", end="")
                 for j in range(self.__size):
-                    print("#", end="")
-            print("")
+                    print("#")
