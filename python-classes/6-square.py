5ef53ca2364044be5005e881d2741b1006ba366e
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-13 19:24:28 -0300

    fix ej 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,1 +1,69 @@
 #!/usr/bin/python3
+"""
+4. Access and update private attribute
+Write a class Square that defines a square by: (based on 3-square.py)
+"""
+
+
+class Square:
+    """doc class"""
+    @property
+    def size(self):
+        """doc"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """doc"""
+        self.__size = value
+
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    # Private instance attribute: position:
+    @property
+    def position(self):
+        """to retrieve it"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """doc"""
+        self.__position = value
+
+        if type(value) != tuple or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Instantiation with optional size and optional position:"""
+        self.__size = size
+        self.__position = position
+
+    def __init__(self, size=0):
+        """Initialize self"""
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """area doc"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print doc"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for k in range(self.__size):
+                    print("#", end="")
+                print()
