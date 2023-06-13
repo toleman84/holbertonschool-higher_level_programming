dfd1ba0f7f743a0ec4e0e4154e96e405d2492d0c
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-12 22:59:10 -0300

    fix ej 0 docstrings

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+0. My first square
+Write an empty class Square that defines a square:
+"""
 
 
 class Square:
+    """
+    Attributes:
+        msg (str): Human readable string describing the exception.
+    """
+
     pass
