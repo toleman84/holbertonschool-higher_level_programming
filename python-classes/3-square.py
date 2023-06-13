2e2ad4be31af22453143b39a59bde7ff6b11dfa5
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-13 00:06:10 -0300

    fix ej 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,12 +8,13 @@ Write a class Square that defines a square by: (based on 2-square.py)
 class Square:
     """doc"""
     def __init__(self, size=0):
+        """constructor"""
         self.__size = size
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
     def area(self):
         return self.__size * self.__size
