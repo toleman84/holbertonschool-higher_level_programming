a459a53d7ffcbf9c181547f63890dce115c908a8
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-13 19:41:36 -0300

    fix ej 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -59,11 +59,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__position[1]):
-                print()
+            if self.__position[1] > 0:
+                print("" * self.__position[1], end="")
             for i in range(self.__size):
-                for j in range(self.__position[0]):
-                    print(" ", end="")
-                for k in range(self.__size):
-                    print("#", end="")
-                print()
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
