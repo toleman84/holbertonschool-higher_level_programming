e4a2c68249dfa84c618736265c328808b3eac29a
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-12 23:33:55 -0300

    fix ej 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+2. Size validation
+Write a class Square that defines a square by: (based on 1-square.py)
+"""
+
+
+class Square:
+    """
+    Attributes:
+        size
+    """
+    def __init__(self, size=0):
+        """function constructor"""
+        self.__size = size
+
+        if type(size) is int:
+            pass
+        else:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
