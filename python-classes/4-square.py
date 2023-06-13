0ec04cb0b31876388379777d6e245d9f5decc1f6
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-13 00:41:48 -0300

    add ej 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+4. Access and update private attribute
+Write a class Square that defines a square by: (based on 3-square.py)
+"""
+
+
+class Square:
+    """doc class"""
+    @property
+    def size(self):
+        """doc"""
+        return self.__size
+
+    @property.setter
+    def size(self, value):
+        """doc"""
+        if type(self.__size) is not int:
+            TypeError("size must be an integer")
+        if self.__size < 0:
+            ValueError("size must be >= 0")
+
+    def __init__(self, size=0):
+        """Initialize self"""
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """area doc"""
+        return self.__size * self.__size
