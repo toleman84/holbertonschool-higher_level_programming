8ab2e4b80da2af3437c4162f92d687d38353b65c
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-12 23:07:31 -0300

    add ej 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+1. Square with size
+Write a class Square that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """
+    Attributes:
+        size
+    """
+    def size(self, size):
+        self.size = size
+
+Square.size()
