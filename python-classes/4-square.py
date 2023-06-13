1cb16998fb88edfbc7f07edbe355975fd5562f35
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-13 00:59:27 -0300

    fix ej 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
         """doc"""
         self.__size = value
 
-        if type(value) is not int:
+        if type(value) != int:
             TypeError("size must be an integer")
         if value < 0:
             ValueError("size must be >= 0")
@@ -25,12 +25,12 @@ class Square:
     def __init__(self, size=0):
         """Initialize self"""
         self.__size = size
-        """
+
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        """
+
     def area(self):
         """area doc"""
         return self.__size * self.__size
