7295917c70697604f20913fa4c5d63e3a777eae0
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-13 15:33:24 -0300

    fix ej 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -37,9 +37,10 @@ class Square:
 
     def my_print(self):
         """print doc"""
-        if self.__size == 0:
+        if (self.__size) == 0:
             print("")
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
                     print("#")
+                print("")
