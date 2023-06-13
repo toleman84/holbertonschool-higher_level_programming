415bb0d1eeab0633e284d78f7c59ba6d2647c7fe
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-13 00:08:41 -0300

    fix ej 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,7 +8,7 @@ Write a class Square that defines a square by: (based on 2-square.py)
 class Square:
     """doc"""
     def __init__(self, size=0):
-        """constructor"""
+        """Initialize self"""
         self.__size = size
 
         if type(size) is not int:
