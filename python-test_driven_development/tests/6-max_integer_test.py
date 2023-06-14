0c4d94ab5ef43fa3a8eff3aa5990631d6f3ca49a
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-14 19:50:20 -0300

    fix ej 0 test

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,9 @@
+import unittest
+from file import function
+
+
+class TestFunction(unittest.TestCase):
+    
+    def max_integer(self):
+        result = max_integer(insertar parámetros de la función)
+        self.assertEqual(result, 2)
