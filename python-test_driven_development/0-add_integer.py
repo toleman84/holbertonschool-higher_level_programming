c134be621e2d80653eaced7f2b292d934438ab9e
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-14 22:22:32 -0300

    fix ej 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,12 +2,12 @@
 def add_integer(a, b=98):
     """doc function"""
     
-    if not isinstance(a, int) or not isinstance(a, float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif isinstance(b, float):
         a = int(a)
 
-    if not isinstance(b, int) or not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     elif isinstance(b, float):
         b = int(b)
