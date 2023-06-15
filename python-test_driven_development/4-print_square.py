2bee2b16d74a55dbdcafde15e41291619819c66a
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-15 19:42:04 -0300

    change to LF

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -13,5 +13,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
+    for i in range(size):   
+        
         print("#" * size)
