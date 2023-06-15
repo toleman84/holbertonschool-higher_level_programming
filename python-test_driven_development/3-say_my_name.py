4e6f2b6d762d18977cbd18a95c05e9f597583a48
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-15 00:08:28 -0300

    fix ej 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,9 +6,9 @@ doc for module
 
 def say_my_name(first_name, last_name=""):
     """doc for function"""
-    if isinstance(first_name, str):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    elif isinstance(last_name, str):
+    elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
