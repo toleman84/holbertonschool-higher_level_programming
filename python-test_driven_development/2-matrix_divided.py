b95eafe9c4e398517ce3c1e47d81b9d13916c88b
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-14 22:56:54 -0300

    add ej 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+doc
+for
+documentation
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    doc for function
+    """
+    if matrix is not isinstance(list, int) or not isinstance(list, float):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    #Each row of the matrix must be of the same size:
+    #    raise TypeError"(Each row of the matrix must have the same size)"
+
+    if not isinstance(div, int) or not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # All elements of the matrix should be divided by div, rounded to 2 decimal places
+    for i in matrix:
+        for j in matrix[i]:
+            print("{:d}".format(matrix[i][j]), end='')
+
+    # return new_matrix
