1e131012c12efdd2fd980aeb00198c9d9056a7b2
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-15 19:49:29 -0300

    fix ej 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -13,7 +13,5 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):   
-        for j in range(size):
-            print("#", end="")
-        print("")
+    for i in range(size):
+        print("#" * size)
