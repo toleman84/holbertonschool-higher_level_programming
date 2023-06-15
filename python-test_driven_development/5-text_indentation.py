8c657dc66d2971b9b99e9cebce5402cbf700cd3b
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-14 21:08:50 -0300

    add ej 4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+doc
+for
+module
+"""
+
+
+def text_indentation(text):
+    """
+    Write a function that prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    else:
+        dic = {'. ': '.\n\n', '? ': '?\n\n', ': ': ':\n\n'}
+        for k, v in dic.items():
+            text = text.replace(k, v)
+        print(text, end='')
