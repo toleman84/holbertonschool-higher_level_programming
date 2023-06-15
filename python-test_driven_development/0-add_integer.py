168c1a3c7b70d6aa16210804d62f5647e9efeca4
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-14 22:23:35 -0300

    fix ej 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,12 +4,14 @@ def add_integer(a, b=98):
     
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    elif isinstance(b, float):
+
+    if isinstance(b, float):
         a = int(a)
 
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    elif isinstance(b, float):
+
+    if isinstance(b, float):
         b = int(b)
 
     return a + b
