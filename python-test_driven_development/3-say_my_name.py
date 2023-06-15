51ac666a65ad291e91df0a28e1f49341207a30eb
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-15 00:07:37 -0300

    add ejs

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+doc for module
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """doc for function"""
+    if isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
