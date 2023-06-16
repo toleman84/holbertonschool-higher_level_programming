d041932f44a3c9835a0fe871d27ed0d4088968e8
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-16 00:31:17 -0300

    add test ej 5

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,9 +1,26 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
 import unittest
-from file import function
+max_integer = __import__('6-max_integer').max_integer
 
 
-class TestFunction(unittest.TestCase):
-    
-    def max_integer(self):
-        result = max_integer(insertar parámetros de la función)
-        self.assertEqual(result, 2)
+class TestMaxInteger(unittest.TestCase):
+    def test_max_integer(self):
+        """test integer"""
+        result = max_integer([1, 2, 3, 4])
+        self.assertEqual(result, 4)
+
+        result2 = max_integer([1, 3, 4, 2])
+        self.assertEqual(result2, 4)
+
+        result3 = max_integer([1])
+        self.assertEqual(result3, 1)
+
+    def test_not_number(self):
+        """test not number"""
+        with self.assertRaises(TypeError):
+            max_integer([1, 'a'])
+
+if __name__ == "__main__":
+    unittest.main()
