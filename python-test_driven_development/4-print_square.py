50d00eb08a22f1ffa12173834c7ad7642a4efcbd
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-16 15:56:04 -0300

    change ej 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -13,5 +13,7 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        print("#" * size)
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    print("#" * size)
