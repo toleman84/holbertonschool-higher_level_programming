558a6539ac04a085d571ba58019b0cd539a0ebe0
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-06-16 15:20:30 -0300

    fix pycodestyle ej 4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,7 +8,7 @@ module
 
 def text_indentation(text):
     """
-    Write a function that prints a text with 2 new lines after each of these characters: ., ? and :
+    prints a text with 2 new lines after each of these characters: ., ? and :
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
