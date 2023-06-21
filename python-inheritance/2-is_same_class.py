af1026e1cb8346c2560d44378ab674c9f9299845
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-21 10:56:41 -0300

    fix doc

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
-    """_summary_
-
-    Args:
-        obj (_type_): _description_
-        a_class (_type_): _description_
-    """
+    """doc"""
     return isinstance(obj, a_class)
