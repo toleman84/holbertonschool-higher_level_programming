1aeeaef5de34509f5c936fd8a2541c927c11c25b
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-21 10:55:24 -0300

    add ej 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+    """
+    return isinstance(obj, a_class)
