7e4fc8aee62993d5b1f9cf9a4b68d8243d8e680d
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-24 20:07:11 -0300

    fix ej 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "ad_item.json"
+filename = "add_item.json"
 
 try:
     obj = load_from_json_file(filename)
