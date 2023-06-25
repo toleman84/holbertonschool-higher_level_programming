f4e170bb06789a77a4d64ac920e773ad0f9cd1d9
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-25 00:10:49 -0300

    fix codestyle ej 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,11 +6,6 @@ class_to_json = __import__("8-class_to_json").class_to_json
 
 class Student:
     """class Student that defines a student by:"""
-    first_name = ""
-    last_name = ""
-    age = 0
-
-
     def __init__(self, first_name, last_name, age):
         """Instantiation with first_name, last_name and age"""
         self.first_name = first_name
