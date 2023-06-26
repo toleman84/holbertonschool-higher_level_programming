17cf78fbf316305c2b2350889008794ae3e88f8b
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-26 13:47:44 -0300

    fix ej 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -39,10 +39,6 @@ class Rectangle(Base):
             Args:
                 width (_type_): _description_
             """
-            if not isinstance(width, int):
-                raise TypeError("width must be an integer")
-            elif width <= 0:
-                raise ValueError("width must be >= 0")
             self.__width = width
 
         @property
@@ -58,10 +54,6 @@ class Rectangle(Base):
             Args:
                 heigth (_type_): _description_
             """
-            if not isinstance(heigth, int):
-                raise TypeError("heigth must be an integer")
-            elif heigth <= 0:
-                raise ValueError("heigth must be >= 0")
             self.__heigth = heigth
 
         @property
@@ -77,10 +69,6 @@ class Rectangle(Base):
             Args:
                 x (_type_): _description_
             """
-            if not isinstance(x, int):
-                raise TypeError("x must be an integer")
-            elif x < 0:
-                raise ValueError("x must be >= 0")
             self.__x = x
 
         @property
@@ -96,8 +84,4 @@ class Rectangle(Base):
             Args:
                 y (_type_): _description_
             """
-            if not isinstance(y, int):
-                raise TypeError("y must be an integer")
-            elif y < 0:
-                raise ValueError("y must be >= 0")
             self.__y = y
