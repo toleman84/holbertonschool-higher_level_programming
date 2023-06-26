c1aa5454f55d8540fa12554112f2c17fb525e631
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-26 19:32:08 -0300

    fix test files for Base test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,25 +4,14 @@
 import unittest
 from models import base
 Base = base.Base
-import pep8
 
 
-class TestBase(unittest.TestCase):
+class TestBase2(unittest.TestCase):
     """_summary_
 
     Args:
         unittest (_type_): _description_
     """
-    def test_pep8(self):
-        """_summary_
-        """
-        style = pep8.StyleGuide()
-        filenames = ['models/base.py']
-        check = style.check_files(filenames)
-        self.assertEqual(check.total_errors, 0, 'PEP8 style errors: %d' % check.total_errors)
-
-
-class TestBase(unittest.TestCase):
     def test_id_given(self):
         """Test ids match when given"""
         self.assertTrue(Base(999), self.id == 999)
