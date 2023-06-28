efe467d846e283c71c777654002ff31c8e7dd29b
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-28 13:45:59 -0300

    fiz

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """_summary_
+
+    Args:
+        Rectangle (_type_): _description_
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """_summary_
+
+        Args:
+            size (_type_): _description_
+            x (int, optional): _description_. Defaults to 0.
+            y (int, optional): _description_. Defaults to 0.
+            id (_type_, optional): _description_. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        clsName = self.__class__.__name__
+        id = self.id
+        x = self.x
+        y = self.y
+        size = self.size
+
+        return ("[{}] ({}) {}/{} - {}".format(clsName, id, x, y, size))
