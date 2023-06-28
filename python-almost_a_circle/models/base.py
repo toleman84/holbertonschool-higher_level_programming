41bb84892b84dd40fc23ac7c4191fcadcbbabec4
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-28 14:43:42 -0300

    add ej 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """_summary_
 """
+import json
 
 
 class Base:
@@ -20,3 +21,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """_summary_
+
+        Args:
+            list_dictionaries (_type_): _description_
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
