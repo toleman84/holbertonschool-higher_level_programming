7b6321df504249739abc4d57c9a8290057ac5089
toleman84 <gdponcemarsiglia@gmail.com>
2023-06-30 20:27:10 -0300

    add tests base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,18 +6,28 @@ from models import base
 Base = base.Base
 
 
-class TestBase2(unittest.TestCase):
+class TestBase(unittest.TestCase):
     """_summary_
 
     Args:
         unittest (_type_): _description_
     """
+    def test_setUp(self):
+        """Test"""
+        pass
+
     def test_id_given(self):
         """Test ids match when given"""
         self.assertTrue(Base(999), self.id == 999)
         self.assertTrue(Base(0), self.id == 0)
         self.assertTrue(Base(1), self.id == 1)
         self.assertTrue(Base(-80), self.id == -80)
+
+    def test_id_not_given(self):
+        """Test"""
+        self.assertTrue(Base(), self.id == 1)
+        self.assertTrue(Base(), self.id == 2)
+
 
 if __name__ == "__main__":
     unittest.main()
