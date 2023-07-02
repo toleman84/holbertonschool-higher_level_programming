01cd3d00aaa98368d4754dce7d9c1f309bb867f1
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-02 17:59:23 -0300

    add tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -22,6 +22,60 @@ class Test_rectangle(unittest.TestCase):
         self.assertTrue(attr.y == 3)
         self.assertTrue(attr.id == 99)
 
+    def test_default_atribute(self):
+        """_summary_
+        """
+        attr = Rectangle(20, 30)
+        self.assertTrue(attr.width == 20)
+        self.assertTrue(attr.height == 30)
+        self.assertTrue(attr.x == 0)
+        self.assertTrue(attr.y == 0)
+        self.assertTrue(attr.id is not None)
+
+    def test_attr_validated(self):
+        """_summary_
+        """
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle("20", 30, 2, 3, 99)
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(20, {"d": 30}, 2, 3, 99)
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(20, 30, None, 3, 99)
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(20, 30, 2, (3, 4), 99)
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(0, 30, 2, 3, 99)
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            Rectangle(20, -30, 2, 3, 99)
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            Rectangle(20, 30, -2, 3, 99)
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            Rectangle(20, 30, 2, -3, 99)
+
+    def test_invalid_args(self):
+        """_summary_
+        """
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, 4, 5, 6, 7)
+
+    def test_class(self):
+        """_summary_
+        """
+        self.assertEqual(type(Rectangle(2, 3)), Rectangle)
+
+    def test_area(self):
+        """_summary_
+        """
+        self.assertEqual(Rectangle(2, 3).area(), 6)
+        self.assertEqual(Rectangle(2, 3, 0, 0).area(), 6)
+        self.assertEqual(Rectangle(2, 3, 0, 0, 12).area(), 6)
+
+    def test_print(self):
+        """_summary_
+        """
+        r = Rectangle(20, 30, 2, 3, 99)
+        self.assertEqual(str(r), '[Rectangle] (99) 2/3 - 20/30')
+
 
 if __name__ == "__main__":
     unittest.main()
