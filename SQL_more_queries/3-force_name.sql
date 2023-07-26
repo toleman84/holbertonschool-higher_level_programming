7d3984093489dd551fb8c842ab9c19617c0f1941
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-26 11:54:46 -0300

    fix ej 3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
 -- script that creates the table force_name on your MySQL server
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
-    name VARCHAR(256));
+    name VARCHAR(256))
+    WHERE name IS NOT NULL;
