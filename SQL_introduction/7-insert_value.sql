92f8f2feb54ea8c9ed3dd909728e52a66c4d29cb
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-25 22:31:22 -0300

    fix ej 7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,5 @@
 -- script that inserts a new row in the table first_table (database hbtn_0c_0) in MySQL server
 -- id = 89
 -- name = Best School
-INSERT INTO first_table (name)
-VALUES (Best School);
+INSERT INTO first_table (id, name)
+VALUES ('89', 'Best School');
