08436fb118c52fdb66372abd42f81603e98aa5bb
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-25 23:07:32 -0300

    add ej 10

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- script that lists all records of the table second_table of the database hbtn_0c_0 in MySQL server
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+
+SHOW score, name
+    FROM second_table,
+    ORDER BY score ASC;
