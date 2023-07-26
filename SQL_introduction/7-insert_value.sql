df16480b66421d5cf5393d0fc0e307fb2241bbb3
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-25 22:29:00 -0300

    add ej 7

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- script that inserts a new row in the table first_table (database hbtn_0c_0) in MySQL server
+-- id = 89
+-- name = Best School
+INSERT INTO first_table (name)
+VALUES (Best School);
