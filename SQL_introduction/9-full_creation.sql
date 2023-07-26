acc3ea271b20f609bddc19162a1fe277b2649170
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-25 22:53:51 -0300

    fix ej 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- asd
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT
-    name VARCHAR(256)
+    id INT,
+    name VARCHAR(256),
     score INT);
 
 INSERT INTO second_table (id, name, score) VALUES ('1', 'John', '10')
