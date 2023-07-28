e17605ae73b520d1a6851f1d590e606fefcc0ed7
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-28 19:53:08 -0300

    fix ej 11

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,8 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 -- If a show doesn’t have a genre, display NULL
-SELECT tv_shows.title, tv_show_genres.genre_id,
-IFNULL(tv_show_genres.genre_id, NULL)
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
+IFNULL(tv_show_genres.genre_id, NULL)
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
