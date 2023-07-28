3779782c526cf6043b74faeb2f2015c23d4e9b71
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-28 20:21:25 -0300

    fix ej 11

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,6 +2,8 @@
 -- If a show doesn’t have a genre, display NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id WHERE IFNULL(tv_show_genres.genre_id, NULL)
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
