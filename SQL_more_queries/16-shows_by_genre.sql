b9058569b976cc32556645712aebff3178b37e26
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-29 19:45:52 -0300

    fix ej 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 
 SELECT tv_shows.title, tv_genres.name
-FROM tv_show
+FROM tv_shows
 
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
