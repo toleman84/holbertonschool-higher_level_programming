5390370ae0fec2936f6155d27678121e0dcfff91
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-29 20:03:48 -0300

    fix ej 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -9,6 +9,4 @@ ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 
-WHERE tv_show_genres.genre_id IS NULL
-
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
