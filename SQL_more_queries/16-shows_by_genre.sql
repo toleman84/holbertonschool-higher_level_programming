7eef3b6e09cbaa3b31c206726cd797cd2120933a
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-29 19:44:44 -0300

    add ej 16

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,14 @@
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show
+
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+
+WHERE tv_show_genres.genre_id IS NULL
+
+ORDER BY tv_show.title ASC, tv_genres.name ASC;
