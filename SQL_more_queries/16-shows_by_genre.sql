b18e1d2caac52f28b39db85bc7f4c6101967841e
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-29 19:58:00 -0300

    add ej 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,10 +3,10 @@
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 
-INNER JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 
 WHERE tv_show_genres.genre_id IS NULL
