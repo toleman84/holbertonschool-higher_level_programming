d0ce108710cae391c518da7816dd06db9e58d399
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-30 17:18:25 -0300

    add ej 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities \
+                INNER JOIN cities \
+                ON cities.state_id = states.id \
+                ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print("{}".format(row))
+
+    cur.close()
+    db.close()
