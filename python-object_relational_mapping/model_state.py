c0f1f32dedda8c565f64aa04eb08e8bc91762de1
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-30 18:57:02 -0300

    fix ej 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ == 'states'
+    __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
