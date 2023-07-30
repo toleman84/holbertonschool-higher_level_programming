3792a9aae7d6a4cfb3ab54ce850154061f985d05
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-30 20:54:22 -0300

    add ej 12

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import State
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    post = State(id=2)
+    session.add(post)
+    session.commit()
+
+    print("{}".format(post.id))
+
+    session.close()
