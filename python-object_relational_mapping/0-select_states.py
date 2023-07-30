2825c5289de78526ee57adba9a8e299af1f16eb1
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-30 00:06:04 -0300

    add ej 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+MY_HOST = 'localhost'
+MY_USER = sys.argv[1]
+MY_PASS = sys.argv[2]
+MY_DB = sys.argv[3]
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host=MY_HOST, port=3306, user=MY_USER, passwd=MY_PASS, database=MY_DB)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows= cur.fecthall()
+
+    for row in rows:
+        print("{}".format(row))
+
+    cur.close()
+    db.close()
