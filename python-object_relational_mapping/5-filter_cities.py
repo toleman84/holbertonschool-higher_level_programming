684d6310288000ac98a62a43bb9bbe1d789c002e
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-30 17:34:52 -0300

    add ej 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name \
+                FROM cities \
+                INNER JOIN states \
+                ON cities.state_id = states.id \
+                WHERE BINARY states.name='{}' \
+                ORDER BY cities.id ASC""".format(sys.argv[4]))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print("{}".format(row))
+
+    cur.close()
+    db.close()
