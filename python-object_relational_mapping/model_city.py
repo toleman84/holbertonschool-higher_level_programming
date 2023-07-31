e2c43a73947958b3d579bf95eee47b791164cc94
toleman84 <gdponcemarsiglia@gmail.com>
2023-07-30 22:36:48 -0300

    add ej 14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Cities in state"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """_summary_
+
+    Args:
+        Base (_type_): _description_
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
